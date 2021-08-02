f4385e917e13a33f4b456b8125ffea96ae452757
ReghinaTurcan <88041351+ReghinaTurcan@users.noreply.github.com>
2021-08-02 11:55:44 +0300

    Create ExSwitchCase.java

---
+++ ExSwitchCase.java
@@ -0,0 +1,20 @@
+
+public class ExSwitchCase {
+	 
+		public static void main(String[] args) {  
+		    //Declaring a variable for switch expression  
+		    int number=20;  
+		    //Switch expression  
+		    switch(number){  
+		    //Case statements  
+		    case 10: System.out.println("8");  
+		  
+		    case 20: System.out.println("7");  
+		  
+		    case 30: System.out.println("30");  
+		  
+		    //Default case statement  
+		    default:System.out.println("Not in 10, 20 or 30");  
+		    }  
+		}  
+		}  
